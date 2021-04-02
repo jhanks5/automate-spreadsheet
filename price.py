ba8fcdc413e3f7fe98db867639786ea6f935ee25
James Hanks <james.hanksjr5@gmail.com>
2021-04-01 23:40:04 -0500

    retrieve price from read-only API endpoint

--- price.py
+++ price.py
@@ -2,10 +2,17 @@ import requests
 import json
 import time
 
-
 response = requests.get('https://api.coinbase.com/v2/prices/BTC-USD/spot')
-data = response.json
-currency = data["data"]["base"]
-price = data["data"]["amount"]
-print(f"Currency: {currency} Price: {price}.")
-time.sleep(5)
+# res = response.status_code
+currencyData = response.json()
+
+currencyTag = currencyData['data']['base']
+currencyPrice = currencyData['data']['amount']
+
+print (currencyTag + ' ' + currencyPrice)
+
+# data = response.json()
+# currency = ['data']['base']
+# price = data
+# print(f"Currency: {currency} Price: {price}.")
+# time.sleep(5)
